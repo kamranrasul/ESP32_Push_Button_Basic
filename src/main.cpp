88580eafe5bb06efffa75c3a32b24c0d847b64e7
kamranrasul <kamran.rasul@gmail.com>
2020-07-21 23:28:33 -0600

    Commented

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,12 @@ void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
+
+  // setting pin 16 as input_pullup pin
   pinMode(16, INPUT_PULLUP);
+
+  // setting the pin 26 as output mode
+  pinMode(26, OUTPUT);
 }
 
 int dvalue = 0;
@@ -12,15 +17,22 @@ int dvalue = 0;
 void loop()
 {
   // put your main code here, to run repeatedly:
+  // scanning for any push on the button
   if (!digitalRead(16))
   {
+    // writing the status of button on the serial
     Serial.println("Pushed");
+    // debounce delay
     delay(150);
 
-    while (!digitalRead(16));
+    // waiting for the button to be released
+    while (!digitalRead(16))
+      ;
+
+    // writing the status of button on the serial
     Serial.println("Released");
 
-    pinMode(26, OUTPUT);
+    // toggling the status of pin 26
     if (dvalue)
     {
       Serial.println("LOW");
